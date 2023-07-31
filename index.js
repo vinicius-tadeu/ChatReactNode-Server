cfe88d5823efe00f82dbd97540c299c637f88211
Vinicius Tadeu Soares Silva <001833@CTJ.ORG.BR>
2023-07-31 13:24:51 -0300

    ajuste de url

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Socket } = require('socket.io')
 
 const app = require('express')()
 const server = require('http').createServer(app)
-const io = require('socket.io')(server, {cors: {origin: 'http://localhost:5173'}})
+const io = require('socket.io')(server, {cors: {origin: 'https://chat-react-node-server.vercel.app/'}})
 
 const PORT = 3001
 
