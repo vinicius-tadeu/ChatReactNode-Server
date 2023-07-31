95af03476417196494e8c60d720e1e25428e448b
vinicius-tadeu <viniciustadeusores@gmail.com>
2023-07-30 22:07:09 -0300

    Backend

---
+++ index.js
@@ -0,0 +1,36 @@
+const { Socket } = require('socket.io')
+
+const app = require('express')()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server, {cors: {origin: 'http://localhost:5173'}})
+
+const PORT = 3001
+
+
+io.on('connection', socket => {
+    console.log('Usuário Conectado!', socket.id);
+
+    socket.on('disconnect', reason =>{
+        console.log('Usuário desconectado!', socket.id)
+    })
+
+    socket.on('set_username', username =>{
+        socket.data.username = username
+        console.log(socket.data.username);
+    })
+    socket.on('message', text =>{
+        const data = new Date()
+        const horas = data.getHours()
+        const minutos = data.getMinutes()
+        const segundos = data.getSeconds()
+        var hhmmmss = [horas, minutos, segundos]
+        io.emit('receive_message', {
+            text,
+            authorId: socket.id,
+            author: socket.data.username,
+            data: hhmmmss.join(':')
+        })
+    })
+})
+
+server.listen(PORT, ()=> console.log('Server running...'))
